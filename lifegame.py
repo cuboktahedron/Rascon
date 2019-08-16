fb4f3b437a809d985e41fd75502212119bd82b90
cuboktahedron <cuboktahedron@gmail.com>
2019-08-16 13:29:16 +0900

    privatize instance variables

--- lifegame.py
+++ lifegame.py
@@ -3,32 +3,32 @@ from datetime import datetime
 
 class LifeGame:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.cells = [[False for x in range(0, width)] for y in range(0, height)]
-        self.fps = 2
+        self.__width = width
+        self.__height = height
+        self.__cells = [[False for x in range(0, width)] for y in range(0, height)]
+        self.__fps = 2
         self._next_ts = datetime.now().timestamp()
 
     def set(self, x, y, status):
-        self.cells[y][x] = status
+        self.__cells[y][x] = status
 
     def get(self, x, y):
-        return self.cells[y][x]
+        return self.__cells[y][x]
 
     def next(self):
         cur_ts = datetime.now().timestamp()
         if cur_ts < self._next_ts:
             return
 
-        self._next_ts = cur_ts + (1 / self.fps)
+        self._next_ts = cur_ts + (1 / self.__fps)
 
-        nextCells = [[False for x in range(0, self.width)] for y in range(0, self.height)]
+        nextCells = [[False for x in range(0, self.__width)] for y in range(0, self.__height)]
 
-        for x in range(0, self.width):
-            for y in range(0, self.height):
+        for x in range(0, self.__width):
+            for y in range(0, self.__height):
                 nextCells[y][x] = self.__next_cell(x, y)
 
-        self.cells = nextCells
+        self.__cells = nextCells
 
     def __next_cell(self, x, y):
         surroundCells = [
@@ -43,22 +43,22 @@ class LifeGame:
         ]
         aliveCount = len(list(filter(lambda cell: cell, surroundCells)))
 
-        if self.cells[y][x]:
+        if self.__cells[y][x]:
             return 2 <= aliveCount <= 3
         else:
             return aliveCount == 3
 
     def __is_alive(self, x, y):
-        x = self.width - 1 if x < 0 else x
-        x = 0 if x >= self.width else x
-        y = self.height -1 if y < 0 else y
-        y = 0 if y >= self.height else y
+        x = self.__width - 1 if x < 0 else x
+        x = 0 if x >= self.__width else x
+        y = self.__height -1 if y < 0 else y
+        y = 0 if y >= self.__height else y
 
-        return self.cells[y][x]
+        return self.__cells[y][x]
 
     def __is_outer(self, x, y):
-        return x < 0 or x >= self.width or y < 0 or y >= self.height
+        return x < 0 or x >= self.__width or y < 0 or y >= self.__height
 
     def get_cells(self):
-        return copy.deepcopy(self.cells)
+        return copy.deepcopy(self.__cells)
 
